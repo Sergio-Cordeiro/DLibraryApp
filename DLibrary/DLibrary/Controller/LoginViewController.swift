613418f01d2899c68d1ce8d9747e664ec3af1ecd
Sergio-Cordeiro <62566660+Sergio-Cordeiro@users.noreply.github.com>
2022-10-02 18:06:55 -0300

    introduce new login screen

--- DLibrary/DLibrary/Controller/LoginViewController.swift
+++ DLibrary/DLibrary/Controller/LoginViewController.swift
@@ -9,8 +9,27 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    func isValidEmail(_ email: String) -> Bool {
+      let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+      let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+      return emailPred.evaluate(with: email)
+    }
 
+    @IBAction func loginButoon(_ sender: Any) {
+//        if isValidEmail(emailText.text!) {
+//            Auth.auth().signIn(withEmail: email, password: password) { (authResult, error) in
+//                
+//            }
+//        }
+    }
+    
+    @IBAction func registerButton(_ sender: Any) {
+    }
 }
